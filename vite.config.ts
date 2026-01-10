2f5a944d084bf07535ae8f0599ed11d2324d0af1
Peter Malope <malopepeter@yahoo.com>
2026-01-10 16:29:58 +0200

    upt

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   // this is the endpoint where the code is going to exist when it's pushed to git pages
   // avoids black screen or 404 after deployment
 
-  base: "/cs-portfolio",
+  base: '/cs-portfolio/',
 
   // in package json add the homepage as the key and full url as the value
   // Syntax -> "homepage": "https://<github-username>.github.io/<repository-name>/"
